2fff7f6bed2f5e518eb5504ed08e9482f312e8d1
nitin032002 <guptanitintd@gmail.com>
2025-07-12 18:35:31 +0530

    add SignUp component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route>
-        
-      </Route>
+      <Route path="/" element= {<Home />} />
+      <Route path="/Profile" element= {<Profile />} />
+      <Route path="/sign-in" element= {<SignIn/>} />
+      <Route path="/sign-up" element= {<SignUp />} />
+      <Route path="/about" element= {<About />} />
     </Routes>
       
     </BrowserRouter>
