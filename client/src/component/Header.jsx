8a1771f804ebc30e1dfb5edc56839eea1c012c36
nitin032002 <guptanitintd@gmail.com>
2025-07-13 01:02:54 +0530

    add Header component and integrate it into App

---
+++ client/src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <span className='text-slate-500'>Luxor</span>
+        <span className='text-slate-700'>Living</span>
+      </h1>
+      
+    </header>
+  )
+}
