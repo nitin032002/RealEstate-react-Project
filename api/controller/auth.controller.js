4053ef54dc09736a727eb15cb2064956baa17a80
nitin032002 <guptanitintd@gmail.com>
2025-07-22 23:15:05 +0530

    feat: add authentication routes and controller for user signup

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,6 @@
+export const signup = (req, res) => {
+
+
+
+
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userrouter from './routes/user.routes.js';
+import authrouter from './routes/auth.routes.js';
 dotenv.config();
 
 mongoose.connect(process.env.Mongo)
